3a055965d4a640d05f01e717441da43b8b69ced1
prajodh <prajodhpragaths@gmail.com>
2025-05-15 17:10:50 -0500

    added service layer and added an api to get element by id

--- src/main/java/com/example/controller/PersonsController.java
+++ src/main/java/com/example/controller/PersonsController.java
@@ -1,17 +1,17 @@
 package com.example.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.example.model.Persons;
-import com.example.repository.PersonsRepository;
-
-import jakarta.websocket.server.PathParam;
-
+import com.example.service.PersonsService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,30 +19,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
-@RequestMapping(path = "/persons")
+@RequestMapping(path = "v1/persons")
 public class PersonsController {
-    
     @Autowired
-    private PersonsRepository personsRepository;
+    private PersonsService personsService;
 
-    @PostMapping("/add")
-    public @ResponseBody String addPersons(@RequestParam String name, @RequestParam Integer age, @RequestParam String sex){
-        Persons person = new Persons();
-        person.setName(name);
-        person.setAge(age);
-        person.setSex(sex);
-        personsRepository.save(person);
-        return "Person saved sucessfully";
+    @PostMapping
+    public @ResponseBody Persons addPersons(@RequestBody Persons person){
+        return personsService.savePerson(person);
     }
 
-    @GetMapping("/all")
-    public @ResponseBody Iterable<Persons> getAllPersons(){
-        return personsRepository.findAll();
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<Persons> getPersonById(@PathVariable Integer id) {
+        return personsService.findPersonById(id);
+    }
+    
+
+    @GetMapping
+    public @ResponseBody Page<Persons> getAllPersons(@RequestParam Integer page, @RequestParam Integer pageSize){
+        return personsService.findAllPerson(page, pageSize);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public @ResponseBody String deletePerson(@PathVariable Integer id){
-        personsRepository.deleteById(id);
-        return "deleted person";    
+        return personsService.deletePerson(id);    
     }
 }
---
+++ src/main/java/com/example/service/PersonsService.java
@@ -0,0 +1,38 @@
+package com.example.service;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.model.Persons;
+import com.example.repository.PersonsRepository;
+
+@Service
+public class PersonsService {
+
+    @Autowired
+    private PersonsRepository personsRepository;
+
+    public Persons savePerson(Persons person){
+        return personsRepository.save(person);
+    }
+
+    public Optional<Persons> findPersonById(Integer id){
+        return personsRepository.findById(id);
+    }
+
+    public Page<Persons> findAllPerson(Integer page, Integer pageSize){
+        Pageable pageable = PageRequest.of(pageSize, pageSize);
+        return personsRepository.findAll(pageable);
+    }
+
+    public String deletePerson(Integer id){
+        personsRepository.deleteById(id);
+        return "Deleted person";
+    }
+}
